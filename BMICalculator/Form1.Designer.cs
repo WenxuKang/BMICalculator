f4c3838ec89dfd3076d053aa65f61b07de40eaeb
300943385 <300943385@PR-E216-41>
2019-07-25 15:51:22 -0400

    interface

--- BMICalculator/Form1.Designer.cs
+++ BMICalculator/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BMICalculator
 {
-    partial class Form1
+    partial class BMI
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Height = new System.Windows.Forms.Label();
+            this.Weight = new System.Windows.Forms.Label();
+            this.Imperial = new System.Windows.Forms.RadioButton();
+            this.Metric = new System.Windows.Forms.RadioButton();
+            this.text1 = new System.Windows.Forms.TextBox();
+            this.text2 = new System.Windows.Forms.TextBox();
+            this.text3 = new System.Windows.Forms.TextBox();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.Height, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Weight, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.Imperial, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.Metric, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.text1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.text2, 1, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 42);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(280, 119);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // Height
+            // 
+            this.Height.Location = new System.Drawing.Point(3, 39);
+            this.Height.Name = "Height";
+            this.Height.Size = new System.Drawing.Size(134, 39);
+            this.Height.TabIndex = 0;
+            this.Height.Text = "My Height";
+            // 
+            // Weight
+            // 
+            this.Weight.Location = new System.Drawing.Point(3, 78);
+            this.Weight.Name = "Weight";
+            this.Weight.Size = new System.Drawing.Size(134, 41);
+            this.Weight.TabIndex = 1;
+            this.Weight.Text = "My Weight";
+            // 
+            // Imperial
+            // 
+            this.Imperial.AutoSize = true;
+            this.Imperial.Location = new System.Drawing.Point(3, 3);
+            this.Imperial.Name = "Imperial";
+            this.Imperial.Size = new System.Drawing.Size(128, 33);
+            this.Imperial.TabIndex = 2;
+            this.Imperial.TabStop = true;
+            this.Imperial.Text = "Imperial";
+            this.Imperial.UseVisualStyleBackColor = true;
+            // 
+            // Metric
+            // 
+            this.Metric.AutoSize = true;
+            this.Metric.Location = new System.Drawing.Point(143, 3);
+            this.Metric.Name = "Metric";
+            this.Metric.Size = new System.Drawing.Size(106, 33);
+            this.Metric.TabIndex = 3;
+            this.Metric.TabStop = true;
+            this.Metric.Text = "Metric";
+            this.Metric.UseVisualStyleBackColor = true;
+            // 
+            // text1
+            // 
+            this.text1.Location = new System.Drawing.Point(143, 42);
+            this.text1.Name = "text1";
+            this.text1.Size = new System.Drawing.Size(100, 38);
+            this.text1.TabIndex = 4;
+            // 
+            // text2
+            // 
+            this.text2.Location = new System.Drawing.Point(143, 81);
+            this.text2.Name = "text2";
+            this.text2.Size = new System.Drawing.Size(100, 38);
+            this.text2.TabIndex = 5;
+            // 
+            // text3
+            // 
+            this.text3.Location = new System.Drawing.Point(66, 292);
+            this.text3.Multiline = true;
+            this.text3.Name = "text3";
+            this.text3.Size = new System.Drawing.Size(175, 121);
+            this.text3.TabIndex = 1;
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(66, 192);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(175, 73);
+            this.Calculate.TabIndex = 2;
+            this.Calculate.Text = "Calculate BMI";
+            this.Calculate.UseVisualStyleBackColor = true;
+            // 
+            // BMI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(304, 441);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.text3);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.MaximizeBox = false;
+            this.Name = "BMI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BMI Calculator";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label Height;
+        private System.Windows.Forms.Label Weight;
+        private System.Windows.Forms.RadioButton Imperial;
+        private System.Windows.Forms.RadioButton Metric;
+        private System.Windows.Forms.TextBox text1;
+        private System.Windows.Forms.TextBox text2;
+        private System.Windows.Forms.TextBox text3;
+        private System.Windows.Forms.Button Calculate;
     }
 }
 
--- BMICalculator/Form1.cs
+++ BMICalculator/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace BMICalculator
 {
-    public partial class Form1 : Form
+    public partial class BMI : Form
     {
-        public Form1()
+        public BMI()
         {
             InitializeComponent();
         }
